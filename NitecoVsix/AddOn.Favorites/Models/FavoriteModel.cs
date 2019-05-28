07310c3cd63dbd3e9895a7a791c86669279108d5
Ha Vu Bui <ha.bui@episerver.com>
2019-05-29 03:15:43 +0700

    Initial

---
+++ NitecoVsix/AddOn.Favorites/Models/FavoriteModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using EPiServer.Data.Dynamic;
+
+namespace $safeprojectname$.Models
+{
+    [EPiServerDataStore(AutomaticallyCreateStore = true)]
+    public class FavoriteModel
+    {
+        public Guid Id { get; set; }
+
+        [EPiServerDataIndex]
+        public string UserName { get; set; }
+
+        [EPiServerDataIndex]
+        public string ContentLink { get; set; }
+    }
+}
